0b28cd56431277074f74883af6a1831ceea2dadc
Alex Molina <l3xmolina@gmail.com>
2018-12-21 23:03:45 -0600

    lesson14: Fixed accordion menu toggle and CSS classes

--- assignments/lesson-14/js/accordionMenu.js
+++ assignments/lesson-14/js/accordionMenu.js
@@ -1,3 +1,8 @@
-$(function() {
-  $( "#accordion" ).accordion();
+$(function(){
+	$('.accordion-toggle .accordion-link').click(function(){
+		if(!$(this).hasClass('accordion-on'))
+		$('.accordion-toggle .accordion-link').removeClass('accordion-on');
+		$(this).toggleClass('accordion-on');
+		$(this).next(".accordion-panel").slideToggle().siblings(".accordion-panel:visible").slideToggle();
+	})
 });
